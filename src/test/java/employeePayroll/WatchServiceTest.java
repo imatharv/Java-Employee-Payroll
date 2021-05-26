d8aa86c37bcb90121eaf4d299ac1808bf853d747
Atharva Joshi <iamatharvaj@google.com>
2021-05-26 17:13:56 +0530

    Added: Solution for use-case-3

---
+++ src/test/java/employeePayroll/WatchServiceTest.java
@@ -0,0 +1,19 @@
+package employeePayroll;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+public class WatchServiceTest
+{
+    @Test
+    public void givenDirectoryWhenWatchedListsAllTheActivites() throws IOException
+    {
+       Path path = Paths.get("C:/Users/iamat/eclipse-workspace/Employee-Payroll/lib");
+       Files.list(path).filter(Files::isRegularFile).forEach(System.out ::println);
+       new WatchServiceExample(path);
+    }
+}
